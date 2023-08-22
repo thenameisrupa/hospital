589c6b4d5f96115e1a127bfc82bc08f32f59e3bf
thenameisrupa <thenameisrupa@gmail.com>
2023-08-22 00:26:47 -0500

    repository file

---
+++ src/main/java/com/io/javaspring/hospital/HospitalRepository.java
@@ -0,0 +1,11 @@
+package com.io.javaspring.hospital;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HospitalRepository extends JpaRepository<Hospital, String> {
+	
+	
+	
+}
